16280efd0c64c516ba3882de97933c411c9a897a
GopalYVkho <gokulkrish1304@gmail.com>
2025-08-31 23:39:52 +0530

    PalindromNumber

---
+++ PalindromeNumber.js
@@ -0,0 +1,20 @@
+function Palindrome(x){
+    if(x<0){
+        return false
+    }
+    let s = String(x)
+    let i = s.length-1;
+    let j = 0;
+    while(j<i){
+        if(s[j] !== s[i]){
+            return false
+        }
+        j++;
+        i--
+    }
+    return true
+}
+
+console.log(Palindrome(1234))
+console.log(Palindrome(121))
+console.log(Palindrome(-121))
