8e32218362fce7b97efc56ee94ce2c9caddc0455
GopalYVkho <gokulkrish1304@gmail.com>
2025-09-01 22:04:30 +0530

    Reverse Integer

---
+++ ReverseInteger.js
@@ -0,0 +1,15 @@
+function reverseInteger(x){
+    var check = x<0?true:false;
+    let convert = String(x);
+    let i=convert.length-1
+    let j =""
+    for(i;i>=0;i--){
+        j=j+convert[i]
+    }
+    return check ===true ? '-'+parseInt(j):parseInt(j)
+
+}
+
+console.log(reverseInteger(123))
+console.log(reverseInteger(-123))
+console.log(reverseInteger(100))
