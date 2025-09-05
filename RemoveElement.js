e87486ae4263502f0f029dbcc424cf08fd0dfed1
GopalYVkho <gokulkrish1304@gmail.com>
2025-09-05 22:51:03 +0530

    Remove Elements

---
+++ RemoveElement.js
@@ -0,0 +1,14 @@
+function RemoveElement(nums,value){
+    let x = 0;
+    let temp
+    for(let i=0;i<nums.length;i++){
+        if(value != nums[i]){
+            nums[x]=nums[i]
+            x++
+        }
+    }
+    return nums
+}
+
+const ddd = RemoveElement([1,2,3,4,5,1,2],1)
+console.log(ddd)
