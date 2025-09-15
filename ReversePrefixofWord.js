5f0c3011bf0c54368b097cdfffc24004ca527281
GopalYVkho <gokulkrish1304@gmail.com>
2025-09-15 23:34:21 +0530

    Reverse prefixWord

---
+++ ReversePrefixofWord.js
@@ -0,0 +1,25 @@
+var reversePrefix = function(word, ch) {
+    let arr = word.split("")
+    let left = 0;
+    let right = -1;
+    for(let i =0 ;i<word.length;i++){
+        if(word[i]==ch){
+            right = i
+            break;
+        }
+    }
+    if (right === -1) return word;
+
+    while(left < right){
+        let temp = arr[right]
+        arr[right]=arr[left]
+        arr[left] = temp
+        left++
+        right--
+    }
+    console.log(arr.join())
+    // return arr.join("")
+};
+
+reversePrefix("abcdefd","d")
+reversePrefix("xyxzxe","z")
