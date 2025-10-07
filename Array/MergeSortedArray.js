dac09518deb9ea03d74d125bea60692e0575f055
GopalYVkho <gokulkrish1304@gmail.com>
2025-10-07 23:42:31 +0530

    Merge Sorted Array

---
+++ Array/MergeSortedArray.js
@@ -0,0 +1,25 @@
+var merge = function (nums1, m, nums2, n) {
+    let p1 = m-1;
+    let p2 = m-1;
+
+    for(let i=m+n-1;i>=0;i--){
+        if(p2<0) break;
+        if(p1>=0&&nums1[p1]>nums2[p2]){
+            console.log(i);
+            nums1[i] = nums1[p1];
+            p1--
+        }else{
+            console.log(i);
+            nums1[i] = nums2[p2];
+            p2--
+        }
+    }
+    console.log(nums1);
+    
+   
+}
+
+merge([1,2,3,0,0,0],3,[2,5,6],3)
+
+
+        
