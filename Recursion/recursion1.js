832fa60211d20339afa12289928123bcceab94fd
GopalYVkho <gokulkrish1304@gmail.com>
2025-10-17 00:11:51 +0530

    Recursion

--- Recursion/recursion1.js
+++ Recursion/recursion1.js
@@ -1,5 +1,6 @@
+// print n to 1 using recursion
 function rex(x){
-    if(x ===0) return;
+    if(x===0) return;
     console.log(x)
     let num =  x -1;
     rex(num)
@@ -7,3 +8,16 @@ function rex(x){
 
 let a=5;
 rex(a)
+
+
+// print 1 to n using recursion
+let n = 5;
+function rexe(x){ 
+    if(x>n) return;
+    console.log(x)
+    let nums =  x+1;
+    rexe(nums)
+}
+
+
+rexe(1)
